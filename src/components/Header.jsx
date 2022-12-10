a0cbadac9d23316063748c76ac7bb1cd097dede7
Santosh Mohapatra <santoshmohapatra232@gmail.com>
2022-12-11 02:56:38 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+const Header = () => {
+  return (
+    <header className='container'>
+        <h2>Rate us - Feedback</h2>
+    </header>
+  )
+}
+
+export default Header
